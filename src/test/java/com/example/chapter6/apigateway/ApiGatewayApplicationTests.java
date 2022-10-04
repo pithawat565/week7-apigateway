e050d19613bf1be37fb038271b2bb7e5d8c4e122
LAB203_54 <pithawat2624@gmail.com>
2022-10-04 10:39:43 +0700

    Initial commit

---
+++ src/test/java/com/example/chapter6/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter6.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
